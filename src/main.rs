d8e8230ef15cb73807a08cab304e792ff3771ccc
molvin <akerstrom.perry@gmail.com>
2019-10-22 14:16:31 +0200

    Updated dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+extern crate winit;
+
+fn main() 
+{
+    use winit::EventsLoop;
+    use winit::Window;
+    use winit::Event;
+    use winit::WindowEvent;
+    use winit::ControlFlow;
+
+    let mut events_loop = EventsLoop::new();
+    let window = Window::new(&events_loop).unwrap();
+    window.set_title("Window");
+
+    events_loop.run_forever(|event|
+     {
+        match event
+        {
+            Event::WindowEvent { event: WindowEvent::CloseRequested, .. } =>
+            {
+                println!("Window exit");
+                ControlFlow::Break
+            },
+            Event::WindowEvent { event: WindowEvent::Refresh, .. } =>
+            {
+                println!("Refresh");
+                ControlFlow::Continue               
+            },           
+            _ =>
+            {
+                
+                ControlFlow::Continue                    
+            },
+        }
+    });
+    
 }
